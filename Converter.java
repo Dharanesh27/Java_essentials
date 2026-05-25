052aceca3073aedc6e2cbf43a026c64db911e509
Dharanesh27 <dharaneshnsds@gmail.com>
2026-05-26 00:02:43 +0530

    celsius to fahrenheit

---
+++ Converter.java
@@ -0,0 +1,7 @@
+class Converter{
+    public static void main(String[] args) {
+        double celsius = 20.0;
+        double fahrenheit = (celsius * 9/5)+32;
+        System.out.println(fahrenheit);   
+    }
+}
