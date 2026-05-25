799c66cbd047f999bb47ddfb16630e68be596519
Dharanesh27 <dharaneshnsds@gmail.com>
2026-05-25 23:45:46 +0530

    Store price of 3 items as double. Calculate and print total bill

---
+++ MyBill.java
@@ -0,0 +1,9 @@
+public class MyBill{
+    public static void main(String[] args){
+        double onion = 21.687;
+        double chicken = 250.58;
+        double masala = 20.87;
+        double Total = onion+chicken+masala;
+        System.out.println("The Total cost is is : "+Total);
+    }
+}
