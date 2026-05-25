af5cd0969d42a4904e764edaf7f1519f18b2620b
Dharanesh27 <dharaneshnsds@gmail.com>
2026-05-25 23:01:15 +0530

    This is basic code for datatypes

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+class DataTypes{
+    public static void main(String[] args){
+        int age = 21;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean isPlaced = false;
+        String Name = "Joe";
+
+        System.out.println("Name : "+Name);
+        System.out.println("age : "+age);
+        System.out.println("marks : "+marks);
+        System.out.println("grade : "+grade);
+        System.out.println("isPlaced : "+true);
+
+    }
+}
