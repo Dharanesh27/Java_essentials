a45934045640051b6c0cdb9bb77d59a51598091a
Dharanesh27 <dharaneshnsds@gmail.com>
2026-05-28 08:02:03 +0530

    for loop

---
+++ PrimeNum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class PrimeNum{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean isPrime = true;
+        System.out.print("Enterthe number : ");
+        int n = sc.nextInt();
+        if(n<=1){
+            isPrime = false;
+        }
+        else{
+            for (int i = 2; i*i < n; i++) {
+                if(n%i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+        }
+        if(isPrime == true){
+            System.out.println(n + " is a prime number");
+        }
+        else{
+            System.out.println(n + " is not a prime number");
+        }
+        
+    }
+}
