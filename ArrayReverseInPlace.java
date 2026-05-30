a9a9f28b319118d1a7eb3620d53a05a0518b5c5f
Dharanesh27 <dharaneshnsds@gmail.com>
2026-05-30 21:45:07 +0530

    1-d array

--- ArrayReverseInPlace.java
+++ ArrayReverseInPlace.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 class ArrayReverseInPlace{
         public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            
+            Scanner sc = new Scanner(System.in);        
             System.out.print("Enter n: ");
             int n = sc.nextInt();
             int [] arr = new int [n];
