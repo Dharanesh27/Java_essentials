6b31ea8a649efe83a65393958faf4851de5ceec5
Dharanesh27 <dharaneshnsds@gmail.com>
2026-06-05 14:47:16 +0530

    Array List

---
+++ ArrayListInput.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+class ArrayListInput{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+        System.out.println(list);
+    }
+}
